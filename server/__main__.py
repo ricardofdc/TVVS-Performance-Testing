d2b673ba27cf028100dc690d1f5e54fc1cd72a0d
Ricardo Cardoso <ricardofdc@live.com>
2021-12-07 10:45:19 +0000

    a

--- server/__main__.py
+++ server/__main__.py
@@ -1,13 +1,16 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import random
 
 hostName = "localhost"
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        time.sleep(0.5) # really bad server
+        #time.sleep(0.5) # really bad server
+        if random.randrange(1,10) == 1:
+            raise Exception("oi")
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
